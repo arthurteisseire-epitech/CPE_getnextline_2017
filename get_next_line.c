462d1ad41cf50816df07af9b72fd2ec90eae39b5
arthur <arthur.teisseire@epitech.eu>
2018-01-06 19:47:21 +0100

    Bootstrap deleted : was useless

---
+++ get_next_line.c
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2017
+** File Name : get_next_line.c
+** File description:
+** by Arthur Teisseire
+*/
+
+#include "get_next_line.h"
+
+char *get_next_line(int fd)
+{
+
+}
---
+++ get_next_line.h
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2017
+** File Name : get_next_line.h
+** File description:
+** by Arthur Teisseire
+*/
+
+#ifndef GET_NEXT_LINE_H
+#define GET_NEXT_LINE_H
+
+#ifndef READ_SIZE
+#	define READ_SIZE 10
+#endif
+
+char *get_next_line(int fd);
+
+#endif
