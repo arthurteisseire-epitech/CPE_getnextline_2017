d0f36523bc6d4b1f11a472c85c74adf836249841
arthur <arthur.teisseire@epitech.eu>
2018-01-08 18:14:24 +0100

    global variable was useless : fixed

--- tests/test_get_next_line.c
+++ tests/test_get_next_line.c
@@ -11,24 +11,12 @@
 #include <fcntl.h>
 #include "get_next_line.h"
 
-int fd = -1;
-
-void open_file(void)
+Test(get_next_line, read_line)
 {
-	fd = open("data.txt", O_RDONLY);
-	cr_redirect_stdout();
-}
-
-void close_file(void)
-{
-	if (fd != -1)
-		close(fd);
-}
-
-Test(get_next_line, read_line, .init = open_file, .fini = close_file)
-{
+	int fd = open("data.txt", O_RDONLY);
 	char *expected = "Confidence is so overrated.";
 	char *got = get_next_line(fd);
 
 	cr_assert_str_eq(got ,expected);
+	close(fd);
 }
--- tests/test_get_next_line_3.c
+++ tests/test_get_next_line_3.c
@@ -11,18 +11,9 @@
 #include <fcntl.h>
 #include "get_next_line.h"
 
-int fd_3 = -1;
-
-void close_file(void);
-
-void open_file_3(void)
+Test(get_next_line, long_text)
 {
-	fd_3 = open("long_text", O_RDONLY);
-	cr_redirect_stdout();
-}
-
-Test(get_next_line, long_text, .init = open_file_3, .fini = close_file)
-{
+	int fd = open("long_text", O_RDONLY);
 	char *expected = "Si on le rencontre peu souvent sur ce blog,\n"
 	"c’est aussi parce qu’il serait difficile de\n"
 	"le conseiller de prime abord à un utilisateur\n"
@@ -31,14 +22,15 @@ Test(get_next_line, long_text, .init = open_file_3, .fini = close_file)
 	"est moins bien et moins vite supporté, il y a moins\n"
 	"d’applications disponibles et il se destine surtout\n"
 	"au monde des serveurs. Mais rien ne dit qu’un jour, vous aussi…\n";
-	char *got = get_next_line(fd_3);
+	char *got = get_next_line(fd);
 	char *res = "";
 
 	while (got) {
 		res = my_realloc(res, got, my_strlen(got));
 		res = my_realloc(res, "\n", 1);
 		free(got);
-		got = get_next_line(fd_3);
+		got = get_next_line(fd);
 	}
 	cr_assert_str_eq(res ,expected);
+	close(fd);
 }
